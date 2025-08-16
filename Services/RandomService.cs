bad1c9ee5d278b4a5efaaca508e6556b6108ab6c
Luciano dos Santos Moreira <lsmoreira21@hotmail.com>
2025-08-16 10:01:10 -0300

    Execicio Parte1Controller

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -4,26 +4,31 @@ using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class RandomService
-	{
-		int seed;
+    public class RandomService
+    {
+        Random _random = new Random();
         TestDbContext _ctx;
-		public RandomService()
+        public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
-
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                .Options;
             _ctx = new TestDbContext(contextOptions);
         }
-        public async Task<int> GetRandom()
-		{
-            var number =  new Random(seed).Next(100);
-            _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
-			return number;
-		}
+        public async Task<string> GetRandom()
+        {
+            var number = _random.Next(100);
 
-	}
+            // Verifica se o número já existe no banco
+            bool exists = await _ctx.Numbers.AnyAsync(n => n.Number == number);
+            if (exists)
+            {
+                return $"O número {number} já foi cadastrado.";
+            }
+
+            _ctx.Numbers.Add(new RandomNumber() { Number = number });
+            await _ctx.SaveChangesAsync();
+            return $"Número {number} cadastrado com sucesso.";
+        }
+    }
 }
